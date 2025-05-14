3e09a73c35cebc925bf97c9846c829a801536b1d
4-Four <65027419+4-Four@users.noreply.github.com>
2025-05-14 22:13:05 +0800

    Update time.js

--- time.js
+++ time.js
@@ -1,6 +1,6 @@
 function BirthDay() {
     window.setTimeout("BirthDay()", 1000);
-        BirthDay = new Date("02/22/2006 00:00:00"); // 修改出生日期
+        BirthDay = new Date("05/15/1999 00:00:00"); // 修改出生日期
         today = new Date();
         timeold = (today.getTime() - BirthDay.getTime());
         sectimeold = timeold / 1000
